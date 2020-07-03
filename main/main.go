01cea5ecc1eca9495d2329aa10e2ffdf59c3b6fa
Damilare Lana <damilarelana@gmail.com>
2020-07-03 03:01:56 +0100

    setup file reader

--- main/main.go
+++ main/main.go
@@ -5,7 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
+
+	cyoa "github.com/damilarelana/goCYOA"
 )
 
 // define flags
@@ -17,20 +18,8 @@ func errMsgHandler(msg string) {
 	os.Exit(1)
 }
 
-// define parser to
-func parseFileData(fileData *json.Decoder) []problem {
-	returnedValue := make([]problem, len(records))
-	for i, record := range records { // iterate over the multi-dimensional slice
-		returnedValue[i] = problem{
-			question: record[0],                    // remember that each record is a 2 element slice [...] that represents a `question, answer` pair
-			answer:   strings.TrimSpace(record[1]), // strings.TrimSpace() helps to remove spaces around answers from the CSV file
-		}
-	}
-	return returnedValue
-}
-
 // openFile() uses the system to open and prep the file for reading
-func openFile() *os.File {
+func openFile(storyFilename *string) *os.File {
 	openedFile, err := os.Open(*storyFilename)
 	if err != nil {
 		errMsgHandler(fmt.Sprintf("Failed to open JSON file: %s\n", *storyFilename))
@@ -45,7 +34,22 @@ func readFile(f *os.File) *json.Decoder {
 	return fileData
 }
 
+// parseFileData parses the jsonData into structData
+func parseFileData(fileData *json.Decoder, story *cyoa.Story) {
+	err := fileData.Decode(&story) // decode and parse the `json data` into `structs data` within the address space &story
+	if err != nil {
+		errMsgHandler(fmt.Sprintf("Failed to parse file data %v\n", *fileData))
+	}
+}
+
 func main() {
 	// parse flags
-	flag.Parse() // required to initialize the specified flags with the Operating system
+	flag.Parse()                          // required to initialize the specified flags with the Operating system
+	var story cyoa.Story                  // initialize the `story	` struct
+	openedFile := openFile(storyFilename) // open file
+	fileData := readFile(openedFile)      // read file content
+	parseFileData(fileData, &story)       // parse file data
+
+	// print content
+	fmt.Printf("%+v\n", story)
 }
